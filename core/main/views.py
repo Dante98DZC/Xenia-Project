75141806a5c6724cd7ad12b1e921aac2c60ac191
Dante98DZC <dante98dzc@gmail.com>
2021-09-21 02:42:44 -0500

    Primer direccionamiento de prueba.

--- core/main/views.py
+++ core/main/views.py
@@ -1,3 +1,7 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
+def myView(request):
+    return HttpResponse("Hola esta es mi primera URL")
