4e744696b1224ac04b8a3a8dc329f56a266e0142
Dante98DZC <dante98dzc@gmail.com>
2021-09-24 04:20:28 -0500

    Nuevas correcciones en los modelos, e implementacion parcial del Django admin.

--- core/express/admin.py
+++ core/express/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-
+from core.express.models import *
 # Register your models here.
+
+admin.site.register(RoomState)
+admin.site.register(Room)
+admin.site.register(Observ)
+admin.site.register(Client)
+admin.site.register(ClientOb)
+admin.site.register(ClientRoom)
+admin.site.register(Departament)
+admin.site.register(Attendants)
+admin.site.register(KindRep)
+admin.site.register(Executive)
+admin.site.register(Responce)
+admin.site.register(Report)
--- core/express/models.py
+++ core/express/models.py
@@ -6,40 +6,39 @@ class RoomState(models.Model):
     room_state = models.CharField(
         max_length=300, verbose_name='Estado Habitación')
 
+
 class Room(models.Model):
-    number = models.CharField(max_length=4, primary_key=True, verbose_name='Número habitación')
+    number = models.CharField(
+        max_length=4, primary_key=True, verbose_name='Número habitación')
     state = models.ForeignKey(RoomState, on_delete=models.CASCADE)
 
-class Client(models.Model):
-    id = models.CharField(max_length=11, primary_key=True, verbose_name='CI/Pasaporte')
-    rooms = models.ManyToManyField(Room, through="ClientRoom", through_fields=("cliente", "habitacion"))
-    first_name = models.CharField(max_length=30, verbose_name='Nombres')
-    last_names = models.CharField(max_length=30, verbose_name='Apellidos')
-    fly = models.CharField(max_length=50, verbose_name='Vuelo')
-    agency = models.CharField(max_length=50, verbose_name='Agencia')
-    arrive_date = models.DateField(default=datetime.now, verbose_name='Fecha LLegada')
-    leave_date = models.DateField(verbose_name='Fecha Salida')
-    
-    class Meta:
-        verbose_name = "Cliente"
-        verbose_name_plural = "Clientes"
-
-        
-class ClientRoom(models.Model):
-    client = models.ForeignKey(Client, on_delete=models.CASCADE)
-    room = models.ForeignKey(Room, on_delete=models.CASCADE)
-    date = models.DateField(default=datetime.now,
-                            verbose_name='Fecha de asignación')
 
 class Observ(models.Model):
-    observ_id = models.CharField(
-        max_length=10, primary_key=True, verbose_name='ID')
     observ_name = models.CharField(max_length=300, verbose_name='Observación')
-    clients = models.ManyToManyField(Client, through='ClientOb')
 
     class Meta:
         verbose_name = "Observación"
         verbose_name_plural = "Observaciones"
+
+
+class Client(models.Model):
+    id = models.CharField(max_length=11, primary_key=True,
+                          verbose_name='CI/Pasaporte')
+    rooms = models.ManyToManyField(
+        Room, through="ClientRoom", through_fields=("client", "room"))
+    first_name = models.CharField(max_length=30, verbose_name='Nombres')
+    last_names = models.CharField(max_length=30, verbose_name='Apellidos')
+    fly = models.CharField(max_length=50, verbose_name='Vuelo')
+    agency = models.CharField(max_length=50, verbose_name='Agencia')
+    arrive_date = models.DateField(
+        default=datetime.now, verbose_name='Fecha LLegada')
+    leave_date = models.DateField(verbose_name='Fecha Salida')
+    observations = models.ForeignKey(
+        Observ, null=True, blank=True, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = "Cliente"
+        verbose_name_plural = "Clientes"
 
 
 class ClientOb(models.Model):
@@ -49,34 +48,57 @@ class ClientOb(models.Model):
         default=datetime.now, verbose_name='Fecha de Observación')
 
 
+class ClientRoom(models.Model):
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    host_date = models.DateField(default=datetime.now,
+                                 verbose_name='Fecha hospedaje')
+
+
 class Departament(models.Model):
-    id_dpt = models.CharField(primary_key=True, verbose_name='ID Dpt')
+    id_dpt = models.CharField(
+        primary_key=True, max_length=10, verbose_name='ID Dpt')
     name_dpt = models.CharField(max_length=50, verbose_name='Nombre Dpt')
     phone_number = models.CharField(max_length=10, verbose_name='Teléfono Dpt')
 
 
 class Attendants(models.Model):
-    ci_attendant = models.CharField()
+    ci_attendant = models.CharField(
+        max_length=11, primary_key=True, verbose_name='Ci Encargado')
     first_name = models.CharField(max_length=30, verbose_name='Nombres')
     last_names = models.CharField(max_length=30, verbose_name='Apellidos')
     dpt = models.ForeignKey(Departament, on_delete=models.CASCADE)
 
 
 class KindRep(models.Model):
-    id_kind = models.CharField(primary_key=True, verbose_name='ID Tipo')
+    id_kind = models.AutoField(primary_key=True , verbose_name='ID Tipo')
     name_kind = models.CharField(max_length=50, verbose_name='Nombre Tipo')
 
 
 class Executive(models.Model):
-    ci_executive = models.CharField(max_length=11, primary_key=True)
+    ci_executive = models.CharField(
+        max_length=11, primary_key=True, verbose_name='CI ejecutivo')
     first_name = models.CharField(max_length=30, verbose_name='Nombres')
     last_names = models.CharField(max_length=30, verbose_name='Apellidos')
-    user_name = models.CharField()
+    user_name = models.CharField(max_length=11, verbose_name='Usuario')
 
 
-class Resolution(models.Model):
+class Responce(models.Model):
+    id_response = models.BigAutoField(primary_key=True , verbose_name='ID Respuesta')
     description = models.CharField(max_length=500, verbose_name='Descripción')
 
 
 class Report(models.Model):
+    report_number = models.AutoField(primary_key=True, verbose_name='Número reporte')
     client_room = models.ForeignKey(ClientRoom, on_delete=models.CASCADE)
+    executive = models.ForeignKey(Executive, on_delete=models.CASCADE)
+    attendant = models.ForeignKey(Attendants, on_delete=models.CASCADE)
+    kind = models.ForeignKey(KindRep, on_delete=models.CASCADE)
+    description = models.CharField(max_length=500, verbose_name='Descripción')
+    get_date_time = models.DateTimeField(
+        default=datetime, verbose_name='Fecha de recivo')
+    com_date_time = models.DateTimeField(
+        default=datetime.now, verbose_name='Fecha de comunicado')
+    response_date_time = models.DateTimeField(
+        default=datetime.now, verbose_name='Fecha de respuesta')
+    responce = models.ForeignKey(Responce, on_delete=models.CASCADE)
