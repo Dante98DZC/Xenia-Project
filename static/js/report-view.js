137e7edc90b515d37f435ff7c612b3e27163b9e2
TheRealVizard <vizard@divineslns.com>
2021-10-11 15:25:04 -0400

    Arreglado error en script

--- static/js/report-view.js
+++ static/js/report-view.js
@@ -1,6 +1,5 @@
 $(function () {
     var $table = $("#table");
-    var currentScript = document.currentScript;
     $("#toolbar")
         .find("select")
         .change(function () {
@@ -10,28 +9,29 @@ $(function () {
             });
         })
         .trigger("change");
-    function buttons() {
-        return {
-            btnAdd: {
-                text: "Add new row",
-                icon: "fa-plus",
-                event: function () {
-                    window.location = currentScript.getAttribute("create-url");
-                },
-                attributes: {
-                    title: "Add a new row to the table",
-                },
-            },
-            btnDelete: {
-                text: "Remove item(s)",
-                icon: "fa-trash",
-                event: function () {
-                    getSelected(currentScript.getAttribute("delete-url"));
-                },
-                attributes: {
-                    title: "Remove item(s)",
-                },
-            },
-        };
-    }
 });
+var currentScript = document.currentScript;
+function buttons() {
+    return {
+        btnAdd: {
+            text: "Add new row",
+            icon: "fa-plus",
+            event: function () {
+                window.location = currentScript.getAttribute("create-url");
+            },
+            attributes: {
+                title: "Add a new row to the table",
+            },
+        },
+        btnDelete: {
+            text: "Remove item(s)",
+            icon: "fa-trash",
+            event: function () {
+                getSelected(currentScript.getAttribute("delete-url"));
+            },
+            attributes: {
+                title: "Remove item(s)",
+            },
+        },
+    };
+}
