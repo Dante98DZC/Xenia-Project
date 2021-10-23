10b849e9f4a43ed5baa3b6cdf724e50e4a0d13da
Dante98DZC <dante98dzc@gmail.com>
2021-10-23 03:53:25 -0500

    Agregando decorador login_required a vista dashboard.

--- config/settings.py
+++ config/settings.py
@@ -139,3 +139,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = 'dashboard'
 
 LOGOUT_REDIRECT_URL = 'login'
+
+LOGIN_URL = 'login'
--- core/main/views.py
+++ core/main/views.py
@@ -12,4 +12,6 @@ class DashboardView(TemplateView):
         context["panel"] = 'Panel de control'
         return context
     
-    
+    @method_decorator(login_required)
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
