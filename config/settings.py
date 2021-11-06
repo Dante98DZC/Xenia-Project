7e37d5fd1b71cbf38aa0aa6145ebf5562652f088
Dante98DZC <dante98dzc@gmail.com>
2021-11-05 23:18:14 -0600

    arreglando .env y cambiando hotel brand.

--- config/settings.py
+++ config/settings.py
@@ -25,8 +25,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
+
+environ.Env.read_env(BASE_DIR / '.env')
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#-smz5)cp0g)%!is0ioetozty9fckn17il_hb-zi0^p55z+@ie'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
