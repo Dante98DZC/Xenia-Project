5d120b031bbdd64d1e3e39b9818ec3bc6097a179
TheRealVizard <vizard@divineslns.com>
2021-11-05 22:56:36 -0400

    Added chart

--- core/main/views.py
+++ core/main/views.py
@@ -1,10 +1,14 @@
 import datetime
 
+import pytz
 from core.express.models import Report
+from dateutil.relativedelta import relativedelta
 from django.contrib.auth.decorators import login_required
-from django.db.models.aggregates import Count, Sum
+from django.db.models.aggregates import Count, Min, Sum
 from django.db.models.query_utils import Q
+from django.utils import timezone
 from django.utils.decorators import method_decorator
+from django.utils.timezone import make_aware
 from django.views.generic import TemplateView
 
 
@@ -14,6 +18,7 @@ class DashboardView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        current_timezone = timezone.get_current_timezone()
         # """
         # {
         #     "today_not_solved_report":"",
@@ -24,24 +29,45 @@ class DashboardView(TemplateView):
         # """
         today_not_solved_report = Count(
             "pk", Q(responsed=False,
-            top_date_time__lte=datetime.datetime.now())
+                    top_date_time__lte=current_timezone.localize(datetime.datetime.now()))
         )
         today_solved_report = Count("pk", Q(responsed=True))
         reports_remaining = Count(
             "pk",
             Q(responsed=False,
-                top_date_time__gte=datetime.datetime.now()
-            )
+                top_date_time__gte=current_timezone.localize(
+                    datetime.datetime.now())
+              )
         )
         today_new_report = Count("pk")
-        today_report = Report.objects.filter(get_date_time__date=datetime.datetime.today()).aggregate(
+        today_report = Report.objects.filter(get_date_time__date=current_timezone.localize(datetime.datetime.today())).aggregate(
             today_not_solved_report=today_not_solved_report,
             today_solved_report=today_solved_report,
             reports_remaining=reports_remaining,
             today_new_report=today_new_report
         )
+        min_date = Report.objects.aggregate(
+            first_date=Min("get_date_time"))["first_date"]
+        midnight = datetime.time(0)
+        range_date = datetime.datetime.combine(min_date.date(), midnight)
+        agree_count = Count("pk", Q(agree=True))
+        not_agree_count = Count("pk", Q(agree=False))
+        agree_series = []
+        not_agree_series = []
+        dates_series = []
+        while range_date <= datetime.datetime.today():
+            next_day = range_date + relativedelta(days=1)
+            day_counts = Report.objects.filter(response_date_time__range=(current_timezone.localize(
+                range_date), current_timezone.localize(next_day))).aggregate(agree_count=agree_count, not_agree_count=not_agree_count)
+            agree_series.append(day_counts["agree_count"])
+            not_agree_series.append(day_counts["not_agree_count"])
+            dates_series.append(str(range_date))
+            range_date = next_day
+
         context["panel"] = "Panel de control"
         context["data"] = today_report
+        context["series"] = {"dates": dates_series,
+                             "agree_series": agree_series, "not_agree_serise": not_agree_series}
 
         return context
 
---
+++ static/js/dashboard.js
@@ -0,0 +1,61 @@
+var chartScript = document.currentScript;
+window.addEventListener("DOMContentLoaded", (event) => {
+    data = JSON.parse(chartScript.getAttribute("data").split("'").join('"'));
+    agree_values = [];
+    not_agree_values = [];
+    labels = [];
+    for (let i = 0; i < data["dates"].length; i += 1) {
+        let current_date = new Date(data["dates"][i]);
+        agree_values.push({
+            x: current_date,
+            y: data["agree_series"][i],
+        });
+        not_agree_values.push({
+            x: current_date,
+            y: data["not_agree_serise"][i],
+        });
+        labels.push(
+            ("0" + current_date.getDate()).slice(-2) +
+                "/" +
+                ("0" + (current_date.getMonth() + 1)).slice(-2) +
+                "/" +
+                current_date.getFullYear()
+        );
+    }
+    var ctx = document.getElementById("myChart").getContext("2d");
+    var chartData = {
+        labels: labels,
+        datasets: [
+            {
+                label: "Inconformes",
+                data: not_agree_values,
+                backgroundColor: "rgba(255, 0, 0, 0.2)",
+                borderColor: "rgba(255, 0, 0, 1)",
+                borderWidth: 1,
+                fill: true,
+            },
+            {
+                label: "Conformes",
+                data: agree_values,
+                backgroundColor: "rgba(0, 255, 0, 0.2)",
+                borderColor: "rgba(0, 255, 0, 1)",
+                borderWidth: 1,
+                fill: true,
+            },
+        ],
+    };
+    var myChart = new Chart(ctx, {
+        type: "line",
+        data: chartData,
+        options: {
+            responsive: true,
+            scales: {
+                xAxes: [
+                    {
+                        type: "time",
+                    },
+                ],
+            },
+        },
+    });
+});
